21890adaca49d6a208e2e0aaf3cdf9b40b33fbd8
YuweiZhang <yuwei.z@duke.edu>
2020-11-22 12:19:11 -0500

    parse iput

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template
-import random
+from flask import Flask,render_template, request
+import random,json
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    input_text = 'I like it'
-    input_text = "Our application provides sentiment analysis for your customized text based on pre-trained " \
-                 "machine learning and deep learning models. The results are in 3 categories: positive, neutral and negative."
-    res = random.choice(['Positive', 'Negative','Neutral'])
-    return render_template('./index.html',input_text=input_text, res = res)
+    input_text = request.values.get('user_text')
+    if input_text:
+        res = random.choice(['Positive', 'Negative','Neutral'])
+    else:
+        input_text = ''
+        res = 'Empty Input'
+    return render_template('./index.html',input_text=input_text, res=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
