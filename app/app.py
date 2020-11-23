6d5156fdc47e6ad5eb4fe0343ca828f8f592da3d
YuweiZhang <yuwei.z@duke.edu>
2020-11-22 21:24:54 -0500

    revise return number & team

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, request
-import random,json
+import random
 
 app = Flask(__name__)
 
@@ -7,11 +7,13 @@ app = Flask(__name__)
 def index():
     input_text = request.values.get('user_text')
     if input_text:
-        res = random.choice(['Positive', 'Negative','Neutral'])
+        res1 = random.choice(['Positive', 'Negative','Neutral'])
+        res2 = random.choice(['Positive', 'Negative', 'Neutral'])
     else:
         input_text = ''
-        res = 'Empty Input'
-    return render_template('./index.html',input_text=input_text, res=res)
+        res1 = 'Empty Input'
+        res2 = 'Empty Input'
+    return render_template('./index.html',input_text=input_text, res1=res1, res2=res2)
 
 if __name__ == '__main__':
     app.run(debug=True)
